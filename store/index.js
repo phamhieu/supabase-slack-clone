2bc538e3b9357dc960b85d2dd858d2c293a28ee1
phamhieu <phamhieu1998@gmail.com>
2021-09-23 15:18:52 +0700

    fix: LokiJs adapter

--- store/index.js
+++ store/index.js
@@ -1,5 +1,6 @@
 import LokiJSAdapter from "@nozbe/watermelondb/adapters/lokijs";
 import { supabase } from "lib/Store";
+import schema from "model/schema";
 import Channel from "model/Channel";
 import Message from "model/Message";
 import RolePermission from "model/RolePermission";
@@ -8,41 +9,8 @@ import UserRole from "model/UserRole";
 
 const adapter = new LokiJSAdapter({
   schema,
-  // (You might want to comment out migrations for development purposes -- see Migrations documentation)
-  migrations,
-  useWebWorker: false,
-  useIncrementalIndexedDB: true,
-  // dbName: 'myapp', // optional db name
-
-  // --- Optional, but recommended event handlers:
-
-  onQuotaExceededError: (error) => {
-    // Browser ran out of disk space -- offer the user to reload the app or log out
-  },
-  onSetUpError: (error) => {
-    // Database failed to load -- offer the user to reload the app or log out
-  },
-  extraIncrementalIDBOptions: {
-    onDidOverwrite: () => {
-      // Called when this adapter is forced to overwrite contents of IndexedDB.
-      // This happens if there's another open tab of the same app that's making changes.
-      // Try to synchronize the app now, and if user is offline, alert them that if they close this
-      // tab, some data may be lost
-    },
-    onversionchange: () => {
-      // database was deleted in another browser tab (user logged out), so we must make sure we delete
-      // it in this tab as well - usually best to just refresh the page
-      if (checkIfUserIsLoggedOut()) {
-        window.location.reload();
-      }
-    },
-  },
+  dbName: "supabaseSlackClone",
 });
-
-function checkIfUserIsLoggedOut() {
-  const session = supabase.auth.session();
-  return !session.user;
-}
 
 // Then, make a Watermelon database from it!
 const database = new Database({
